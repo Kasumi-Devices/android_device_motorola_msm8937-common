fc3f846758cd4d6b37a8e1e973116d137d517122
Nick <nift4@protonmail.com>
2021-10-16 17:01:45 +0200

    init.extra: update config once again

--- rootdir/bin/init.extra.sh
+++ rootdir/bin/init.extra.sh
@@ -1,5 +1,5 @@
 ### extras ###
 # Override CAF's lowmemorykiller values with better ones for both 2 and 3GB RAM models
-echo 4710,9420,14130,18840,23550,28260 > /sys/module/lowmemorykiller/parameters/minfree
-# Set swapiness to 90. We only use super-cheap (of CPU time) ZRAM.
-echo 90 > /proc/sys/vm/swappiness
+echo 25601,51202,115203,256004,358405,384006 > /sys/module/lowmemorykiller/parameters/minfree
+# Set swapiness to 120. We only use super-cheap (of CPU time) ZRAM.
+echo 120 > /proc/sys/vm/swappiness
