36ba255d055246091d7e43400a4660ad4eb6da64
Nick <nift4@protonmail.com>
2021-12-18 15:42:10 +0100

    reset lmk

--- rootdir/bin/init.extra.sh
+++ rootdir/bin/init.extra.sh
@@ -1,5 +1,1 @@
 ### extras ###
-# Override CAF's lowmemorykiller values with better ones for both 2 and 3GB RAM models
-echo 25601,51202,115203,256004,358405,384006 > /sys/module/lowmemorykiller/parameters/minfree
-# Set swapiness to 120. We only use super-cheap (of CPU time) ZRAM.
-echo 120 > /proc/sys/vm/swappiness
